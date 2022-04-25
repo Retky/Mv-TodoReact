a75a0eaec762bfff4b058f0f42cbb5b61deaceba
Retky <jose.arredondo.a@outlook.com>
2022-04-25 13:38:28 -0500

    Add 'todoList' element
    To generate list base on state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import TodoContainer from "./components/TodoContainer"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TodoContainer from "./components/TodoContainer";
 
 ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <TodoContainer /></React.StrictMode>);
