aa28d67770352535ccdcd49cbeced140260714d4
Retky <jose.arredondo.a@outlook.com>
2022-04-25 17:42:58 -0500

    Add 'unmount'

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -25,6 +25,10 @@ class TodoItem extends React.Component {
     }
   }
 
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
+
   render() {
     let viewMode = {}
     let editMode = {}
