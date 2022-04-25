ff511b423440c3f395322d09b67f029df40c9191
Retky <jose.arredondo.a@outlook.com>
2022-04-25 14:11:40 -0500

    Modify 'handleChange' syntax for strict-mode & ES6

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,13 +24,18 @@ class TodoContainer extends React.Component {
   }
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        };
-        return todo;
-      })
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          };
+          return todo;
+        })
+      }
     });
   };
 
