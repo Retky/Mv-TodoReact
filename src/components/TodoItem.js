9c056396e264cee04472dbacad739b1dd7c051e0
Retky <jose.arredondo.a@outlook.com>
2022-04-25 14:19:49 -0500

    Add 'delelete' btn for TodoItems

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,9 @@ class TodoItem extends React.Component {
       <input type="checkbox"
       checked={this.props.todo.completed}
       onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
       {this.props.todo.title}
     </li>
   }
