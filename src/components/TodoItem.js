2e336c304f91634897003a0c6d6051b739312987
Retky <jose.arredondo.a@outlook.com>
2022-04-25 14:01:31 -0500

    Add 'handleChange' fucntion for checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from "react";
 class TodoItem extends React.Component {
   render() {
     return <li>
-      <input type="checkbox" checked={this.props.todo.completed} /> {this.props.todo.title}
+      <input type="checkbox"
+      checked={this.props.todo.completed}
+      onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
+      {this.props.todo.title}
     </li>
   }
 }
