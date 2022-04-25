c0c97c4cefdedda354a09f3967750c0229b2a3e1
Retky <jose.arredondo.a@outlook.com>
2022-04-25 15:06:06 -0500

    Add 'todos' entire display style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,26 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
+
+const completedStyle = {
+  fontStyle: "italic",
+  color: "#595959",
+  opacity: 0.4,
+  textDecoration: "line-through",
+}
 
 class TodoItem extends React.Component {
   render() {
-    return <li>
+    return <li className={styles.item}>
       <input type="checkbox"
+      className={styles.checkbox}
       checked={this.props.todo.completed}
       onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
       <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
         Delete
       </button>
-      {this.props.todo.title}
+      <span style={this.props.todo.completed ? completedStyle : null}>
+        {this.props.todo.title}
+      </span>
     </li>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoContainer from "./components/TodoContainer";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <TodoContainer /></React.StrictMode>);
