4b4a4c674ce30e9d4f0f69f9fce41b56e7006e3a
Retky <jose.arredondo.a@outlook.com>
2022-04-25 12:12:20 -0500

    Add 'index' text by js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const element = <h1>Hello from Create React App</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
