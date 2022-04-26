783aff562c3ff09e937b4a30426e7e4e54776078
Retky <jose.arredondo.a@outlook.com>
2022-04-26 08:04:49 -0500

    Add 'navbar' with links

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import NotMatch from "./pages/NotMatch";
-
+import Navbar from "./components/Navbar";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Navbar />
       <Routes>
       <Route path="/" element={<TodoContainer />} />
       <Route path="/about" element={<About />} />
