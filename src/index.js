fe9af8c94ab1e3222ffb4ce9b324c39474f69304
Retky <jose.arredondo.a@outlook.com>
2022-04-25 21:29:21 -0500

    Add 'TodoContainer' route

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <TodoContainer /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<TodoContainer />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>);
