f97fda8220c3de2afadccbec0320cd265474b4ac
Retky <jose.arredondo.a@outlook.com>
2022-04-26 07:43:04 -0500

    Add pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import NotMatch from "./pages/NotMatch";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<TodoContainer />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotMatch />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>);
